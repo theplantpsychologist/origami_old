bef5a6fa5aadd778d921dd49747a239334b11f30
theplantpsychologist@gmail.com <theplantpsychologist>
2023-01-31 15:19:31 -0500

    bug fix

--- resources/src/main.js
+++ resources/src/main.js
@@ -39,14 +39,14 @@ export const MAIN = {
             for(const vertex of face.vertices){
                 var index = vertices_coords.findIndex(item => item[0]==vertex.x & item[1]==vertex.y)
                 if(index == -1){index = vertices_coords.length; vertices_coords.push([vertex.x,vertex.y])}
-                else{facevertices.push(index)}
+                facevertices.push(index)
             }
             for(const crease of face.creases){
                 if(crease.mv == 'A' | crease.mv == 'E'){edges_assignment.push("B")}
                 else{edges_assignment.push(crease.mv)}
                 var index1 = vertices_coords.findIndex(item => item[0]==crease.vertices[0].x & item[1]==crease.vertices[0].y)
                 var index2 = vertices_coords.findIndex(item => item[0]==crease.vertices[1].x & item[1]==crease.vertices[1].y)
-                edges_vertices.push([index1,index2])
+                if(edges_vertices.findIndex(item =>haveSameContents(item,[index1,index2])) != -1){edges_vertices.push([index1,index2])}
             }
             faces_vertices.push(facevertices)
         }
@@ -56,7 +56,7 @@ export const MAIN = {
             "edges_assignment": edges_assignment,
             "faces_vertices":faces_vertices
         }
-
+        console.log(cpobject)
         return MAIN.compute_cells(MAIN.process_file(cpobject));
     },
 
