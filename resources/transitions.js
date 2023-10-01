c20907d1a8eff6fff7b653d8940b95f733e209b0
theplantpsychologist@gmail.com <theplantpsychologist>
2023-09-30 21:34:22 -0400

    idk

--- resources/transitions.js
+++ resources/transitions.js
@@ -10,8 +10,8 @@ Start building main input ui
  - both numerical input and mouse input
  - integer grid lines with snapping
 
-Start building general algorithm
- - 
+Instead of passing in so many parameters, clump a,b,av,bv,creases,extraV,startmv into a state dictionary?
+then pack and unpack in every function
 
 Start thinking about how to generalize for full dense bouncing solving
  - How will crease data be represented as?
@@ -22,10 +22,19 @@ make a and be be lists of ordered pairs. x coord is position along the ridge.
 y coord is distance from ridge along axial. then use sin/cos to get coord
 */
 
-function extend(av,bv,creases,startmv,theta,buffer = 1){
+function extend(state,buffer = 1){
     `
     Inputs a bunch of vertices and turns it into a normalized cp object
     `
+    console.log("extend",state)
+
+    av = state.av
+    bv = state.bv
+    const theta = state.theta
+    creases = state.creases
+    extraV = state.extraV
+    const startmv = state.startmv
+
     newVertices = []
     newCreases = []
     //find the max and mins (bounding box)
@@ -42,15 +51,14 @@ function extend(av,bv,creases,startmv,theta,buffer = 1){
     }
     maxy = Math.max(maxy,-1*miny) + buffer
     miny = -1*maxy
-    console.log("minx,maxx,miny,maxy",minx,maxx,miny,maxy)
+    // console.log("minx,maxx,miny,maxy",minx,maxx,miny,maxy)
 
 
     //horizontal lines at beginning and end of transition
-    leftEnd = new Vertex(minx,0)
     rightEnd = new Vertex(maxx,0)
-    newCreases.push(new Crease(leftEnd, av[0].y==0? av[0]:bv[0] ,startmv))
+    // newCreases.push(new Crease(leftEnd, av[0].y==0? av[0]:bv[0] ,startmv))
     newCreases.push(new Crease(rightEnd, av[av.length-1].y==0? av[av.length-1]:bv[bv.length-1], av.length%2==1?othermv:startmv))    
-    newVertices.push(leftEnd,rightEnd)
+    newVertices.push(rightEnd)
     
     //extend pleat lines for each vertex of a and b
     var currentmv = startmv
@@ -68,11 +76,7 @@ function extend(av,bv,creases,startmv,theta,buffer = 1){
         newVertices.push(newVertex)
     }
     //merge and cleanup
-    allVertices = av.concat(newVertices) 
-    for(const vertex of bv){
-        if(!av.includes(vertex)){av.push(vertex)}
-    }
-    console.log(5,allVertices)
+    allVertices = av.concat(newVertices).concat(bv).concat(extraV)
 
     allCreases = creases.concat(newCreases)
     for(const crease of allCreases){
@@ -80,18 +84,30 @@ function extend(av,bv,creases,startmv,theta,buffer = 1){
             vertex.creases.push(crease)
         }
     }
+
+    for(i=0;i<allVertices.length;i++){
+        for(j=i+1;j<allVertices.length;j++){
+            if (allVertices[i] === allVertices[j]){
+                console.log('removed')
+                allVertices.splice(j,1)
+                break
+            }
+        }
+    }
+
     cp = new CP(allVertices,allCreases)
-    console.log(6,structuredClone(cp.vertices))
     //normalize to the unit square, with the horizontal line centered along y=0.5
     //watch out for cases that are taller than they are wide (may need to change html dimensions)
     //minx goes to 0, maxx goes to 1
+    
     var scale = 1/(maxx-minx)
     for(const vertex of cp.vertices){
-        console.log(vertex.x-minx,vertex)
+        // console.log(vertex.x-minx,vertex)
         vertex.x = 0 + (vertex.x-minx)*scale
+        // vertex.y += 0.5
+        // vertex.y = vertex.y - (vertex.y-0.5)* scale
         vertex.y = (vertex.y*scale) + 0.5
     }
-    console.log(7,structuredClone(cp.vertices))
     return cp
 }
 function pythagorean(a,b){
@@ -99,26 +115,48 @@ function pythagorean(a,b){
 }
 
 //=======================================
-function start(){
+function start(a,b){
     `
     Main function to solve the transition.
     Inputs (fed in from html): a,b,theta,startmv
     output: cp file, display and/or download
     `
+    console.log('=====STARTING=====')
     //setup
     paper.project.clear()
-    const theta = parseFloat(angle.value) * (2*Math.PI)/360
-    const ainput = JSON.parse(topInput.value).sort()
-    const binput = JSON.parse(bottomInput.value).sort()
-    var a = structuredClone(ainput)
-    var b = structuredClone(binput)
-    const startmv = mvInput.value
-    const othermv = mvInput.value == "M"?"V":"M"
-    var creases = [] //list of crease objects, except for the boundary ones
-    var av = [] //list of vertex objects for a
-    var bv = [] //list of vertex objects for b
+    var state = {
+        theta:parseFloat(angle.value) * (2*Math.PI)/360,
+        ainput:a,//JSON.parse(topInput.value).sort() ,
+        binput:b,//JSON.parse(bottomInput.value).sort(),
+        startmv:mvInput.value,
+        mv:mvInput.value,
+        othermv:mvInput.value =="M"?"V":"M",
+        creases:[],
+        extraV:[],
+        av:[],
+        bv:[],
+    }
+    state.a = structuredClone(state.ainput)
+    state.b = structuredClone(state.binput)
 
-    step1(a,b,av,bv)
+    state.root = new Vertex(Math.min(state.a[0],state.b[0])-1,0)
+    state.extraV.push(state.root)
+
+    // const theta = parseFloat(angle.value) * (2*Math.PI)/360
+    // const ainput = JSON.parse(topInput.value).sort() //SWAPPED
+    // const binput = JSON.parse(bottomInput.value).sort()
+    // var a = structuredClone(ainput)
+    // var b = structuredClone(binput)
+    // const startmv = mvInput.value
+    // const othermv = mvInput.value == "M"?"V":"M"
+    // var creases = [] //list of crease objects, except for the boundary ones
+    // var extraV = []
+
+    // var av = [] //list of vertex objects for a
+    // var bv = [] //list of vertex objects for b
+    
+    state = step1(state)
+    console.log("done", state)
     // console.log(av,bv)
     //step1
         //within step1, call step2
@@ -145,194 +183,218 @@ function start(){
     //     new Crease(av[1],bv[0],"V")
     // ]
 
-    cp = extend(av,bv,creases,startmv,theta)
+    cp = extend(state)
     displaycp = displayCp(cp,10,10,490,490)
+    console.log('=====FINISHED=====')
 }
 
-function step1(a,b,av,bv,theta){
+function step1(state){
     `
     two creases approaching the ridge. 
+    root is the previous vertex, if applicable
     `
-    if(a.length == 0 | b.length ==0){console.log('DONE');return} //base case
+    a = state.a
+    b = state.b
+    av = state.av
+    bv = state.bv
+    theta = state.theta
+    creases = state.creases
+    extraV = state.extraV
+    mv = state.mv
+    root = state.root
+    othermv = state.othermv
+
+    // othermv = mv=="M"?"V":"M"
+    if(a.length == 0 | b.length ==0){console.log('DONE');return state} //base case
     if(eq(a[0],b[0])){
-        console.log("EQUAL case")
+        console.log("step1 equal case")
         vertex = new Vertex(a[0],0)
         av.push(vertex)
         bv.push(vertex)
         a.splice(0,1)
         b.splice(0,1)
-        step1(a,b,av,bv)
+        creases.push(new Crease(vertex,root,mv))
+        state.a = a
+        state.b = b
+        state.av = av
+        state.bv = bv
+        state.creases = creases
+        state.mv = othermv
+        state.root = vertex
+        state.othermv = mv
+        return step1(state)
+        
     } else if (a[0]<b[0]){
-        root = new Vertex(a[0],0)
-        av.push(root)
+        console.log("step1 a is closer")
+        vertex = new Vertex(a[0],0)
+        av.push(vertex)
         a.splice(0,1)
-        step2(a,b,av,bv,root,theta)
-    } else if (a[0]>b[0]){
-        root = new Vertex(b[0],0)
-        bv.push(root)
+        creases.push(new Crease(vertex,root,mv))
+        // step2(a,b,av,bv,theta,creases,extraV,vertex,othermv)
+    } else if (a[0]>=b[0]){
+        console.log("step1 b is closer")
+        vertex = new Vertex(b[0],0)
+        bv.push(vertex)
         b.splice(0,1)
-        step2(a,b,av,bv,root,theta)
+        creases.push(new Crease(vertex,root,mv))
+        // step2(a,b,av,bv,theta,creases,extraV,vertex,othermv)
     }
+    state.a = a
+    state.b = b
+    state.av = av
+    state.bv = bv
+    state.creases = creases
+    state.root = vertex
+    state.mv = mv
+    state.othermv = othermv
+    return step2(state)
 }
 
-function step2(a,b,av,bv,root,theta){
+function step2(state){
     `
-    Takes a root and branches off
+    passed in: after step 1 ridge has just hit one crease, now split and make two creases, one that hits a0 and one that hits b0
+
+    return the new vertices that result from hitting a0 and b0, and all of the old stuff
+
+    test case: [-1,0,1,2] and [-1,1]
     `
-    if(a.length == 0 | b.length ==0){return}
-    console.log('step 2')
+    a = state.a
+    b = state.b
+    av = state.av
+    bv = state.bv
+    theta = state.theta
+    root = state.root
+    creases = state.creases
+    extraV = state.extraV
+    mv = state.mv
+    othermv = state.othermv
+
+    if(a.length == 0 | b.length ==0){return state}
     if(a[0]<b[0]){
-
+        console.log('step 2 a is closer')
+        //send perpendicular line to b. vertical line to a
+        newb = new Vertex(root.x,-Math.tan(theta)*(b[0]-root.x))
+        newa = new Vertex(a[0]-(a[0]-root.x)*(Math.cos(theta))**2 , Math.cos(theta)*Math.sin(theta)*(a[0]-root.x))
     } else{
-
+        console.log('step 2 b is closer')
+        //send perpendicular line to a, vertical line to b
+        newa = new Vertex(root.x,Math.tan(theta)*(a[0]-root.x))
+        newb = new Vertex(b[0]-(b[0]-root.x)*(Math.cos(theta))**2 , -Math.cos(theta)*Math.sin(theta)*(b[0]-root.x))
     }
+    // console.log(a,b)
+    // console.log(root,a[0],b[0],newa,newb)
+    bv.push(newb)
+    b.splice(0,1)
+    av.push(newa)
+    a.splice(0,1)
+
+    othermv = mv=="M"?"V":"M"
+    creases.push(new Crease(root,newa,othermv))
+    creases.push(new Crease(root,newb,othermv))
+    creases.push(new Crease(newa,newb,mv))
+
+    state.a = a
+    state.b = b
+    state.av = av
+    state.bv = bv
+    state.creases = creases
+    state.subroots = [newa,newb]
+    state.mv = othermv
+    state.othermv = mv
+    return step3(state)
+}
+function step3(state){
+    `
+    send out tentative creases from each subroot to make them flat foldable.
+    Splits off into 3a,3b,3c depending where a[0] and b[0] hit the tentative creases.
+    `
+    a = state.a
+    b = state.b
+    suba = state.subroots[0]
+    subb = state.subroots[1]
+
+    // if(a.length == 0 | b.length ==0){return state}
+
+    if (suba.x > subb.x){
+        console.log("step3: a is further")
+        tentative = new Vertex(
+            suba.y * (suba.y - subb.y)/(suba.x-subb.x),// suba.x + (suba.y - subb.y),
+            0// suba.y - (suba.x - subb.x)
+        )
+    } else if (subb.x > suba.x){
+        console.log("step3: b is further")
+        tentative = new Vertex(
+            subb.y * (subb.y - suba.y)/(subb.x-suba.x),// subb.x - (subb.y - suba.y),
+            0// subb.y + (subb.x - suba.x)
+        )
+    } else{console.log("step 3: Something went wrong"); return state}
+
+    const x = tentative.x
+
+    // console.log(a,b,tentative)
+    if((a[0]>x & b[0]>x) | (eq(a[0],x)&eq(b[0],x))){
+        state.creases.push(new Crease(tentative,suba,mv))
+        state.creases.push(new Crease(tentative,subb,mv))
+        state.extraV.push(tentative)
+        return step3a(state,tentative)
+    }
+    if((eq(a[0],x) & b[0]>x) | (eq(b[0],x) & a[0]>x)){
+        state.creases.push(new Crease(tentative,suba,mv))
+        state.creases.push(new Crease(tentative,subb,mv))
+        return step3b(state,tentative)
+    }
+    if(b[0]<x & a[0]<x){
+        step3d()
+        return state
+    }
+    else{
+        step3c()
+        return state
+    }
+    
+
+    // creases.push(new Crease(tentative,suba,mv))
+    // creases.push(new Crease(tentative,subb,mv))
 
 }
-function step3(){
+function step3a(state,tentative){
     `
-    Splits off into 3a,3b,3c
+    test case: [-1,0,1,3] and [-1,1,3,4]
+
+    [-1,1,3,4,5,6] and [-1,0,1,3,4,5,6]
     `
+    console.log("step 3a")
+    x = tentative.x
+    if(eq(a[0],x)&eq(b[0],x)){
+        state.mv = mv=="M"?"V":"M"
+        state.av.push(tentative)
+        state.bv.push(tentative)
+        state.av.splice(0,1)
+        state.bv.splice(0,1)
+    }
+    state.root = tentative
+    return step2(state)
 }
 
-function simpleCase(){
-    paper.project.clear()
-    const theta = parseFloat(angle.value) * (2*Math.PI)/360
-    const a = JSON.parse(topInput.value).sort()
-    const b = JSON.parse(bottomInput.value).sort()
-    const startmv = mvInput.value
-    const othermv = mvInput.value == "M"?"V":"M"
-    const minlength = (a[2]-a[0])/2
-    console.log(a[0],a[1],a[2],b[0], b[0] == a[0] - a[1] + a[2])
-
-    b0 = new Vertex(a[0],a[0]-b[0]*Math.tan(theta))
-    a1 = new Vertex(a[0] + (a[1]-a[0])*Math.sin(theta)**2,(a[1]-a[0])*Math.sin(theta)*Math.cos(theta))
-    alpha = theta - Math.atan2(a1.x-b0.x,a1.y-b0.y)
-    above = a[0]+(b[0]-a[0])*Math.tan(theta)/Math.tan(alpha)
-    below = a[0] + (a[1]-a[0])*Math.sin(theta)**2 + (a[1]-a[0])*Math.sin(theta)*Math.cos(theta)/Math.tan(theta-alpha)
-    console.log(above,below)
-
-    a0 = new Vertex(a[0],0)
-    a2 = new Vertex(a[2],0)
-    var creases = []
-    var vertices = [a0,a2,b0,a1]
-    creases.push(...[
-        new Crease(a0,new Vertex(a[0]-minlength,0),startmv),
-        new Crease(a0,new Vertex(a0.x - minlength*Math.cos(theta),minlength*Math.sin(theta)),othermv),
-        new Crease(a0,a1,othermv),
-        new Crease(a0,b0,othermv),
-        new Crease(b0,a1,startmv),
-        new Crease(a1,new Vertex(a1.x-minlength*Math.cos(theta),a1.y+minlength*Math.sin(theta)),startmv),
-        new Crease(b0,new Vertex(b0.x-minlength*Math.cos(theta),b0.y-minlength*Math.sin(theta)),othermv),
-        new Crease(b0,a2,othermv),
-        new Crease(a1,a2,startmv),
-        new Crease(a2,new Vertex(a[2]+minlength,0),othermv),
-        new Crease(a2,new Vertex(a2.x-minlength*Math.cos(theta),minlength*Math.sin(theta)),othermv)
-    ])
-    for(const crease of creases){
-        for(const vertex of crease.vertices){
-            if(!vertices.includes(vertex)){
-                vertices.push(vertex)
-            }
-            if(vertex.creases == []){
-                vertex.creases = [crease]
-            } else{
-                vertex.creases.push(crease)
-            }
-        }
+function step3b(state,tentative){
+    `
+    test case: [0,1,2,3,4] and [0,2,4]
+    `
+    console.log("step 3b")
+    if (eq(a[0],tentative.x)){
+        av.push(tentative)
+        a.splice(0,1)
+    } else{
+        bv.push(tentative)
+        b.splice(0,1)
     }
-
-    cp = new CP(vertices,creases)
-    //console.log(cp)
-    normalize(cp)
-    //console.log(cp)
-    displaycp = displayCp(cp,10,10,490,490)
+    state.root = tentative
+    return step1(state)
 }
 
-function simpleCase2(){
-    //setup
-    paper.project.clear()
-    const theta = parseFloat(angle.value) * (2*Math.PI)/360
-    const a = JSON.parse(topInput.value).sort()
-    const b = JSON.parse(bottomInput.value).sort()
-    const startmv = mvInput.value
-    const othermv = mvInput.value == "M"?"V":"M"
-    var av = [] //coordinates for input points a
-    var bv = []
-    for(var i=0;i<b.length;i++){
-        bv.push([b[i],0])
-    }
-    for(var i=0;i<a.length;i++){
-        av.push([a[i],0])
-    }
-
-    av[0][1] = 0
-    av[2][1] = 0
-    bv[0][1] = pythagorean(b[0]-a[0],b[0]-a[0]*Math.tan(theta))
-    av[1][1] = Math.cos(theta)*(av[1][0]-av[0][1])
-    alpha = theta - Math.atan2(av[1][0]-bv[0][0],av[1][1]-bv[0][1])
-    console.log(av,bv)
-    console.log(alpha * 360/(2*Math.PI))
-
-
-    above = a[1][1] * Math.sin(alpha)/Math.sin(theta-alpha)
-    below = b[0][1] * Math.sin(theta-alpha)/Math.sin(alpha)
-    console.log(above,below)
-
-    //then convert a and b to actual vertices. establish their connections somehow
-
-    var creases = []
-    var vertices = [a0,a2,b0,a1]
-    creases.push(...[
-        new Crease(a0,new Vertex(a[0]-minlength,0),startmv),
-        new Crease(a0,new Vertex(a0.x - minlength*Math.cos(theta),minlength*Math.sin(theta)),othermv),
-        new Crease(a0,a1,othermv),
-        new Crease(a0,b0,othermv),
-        new Crease(b0,a1,startmv),
-        new Crease(a1,new Vertex(a1.x-minlength*Math.cos(theta),a1.y+minlength*Math.sin(theta)),startmv),
-        new Crease(b0,new Vertex(b0.x-minlength*Math.cos(theta),b0.y-minlength*Math.sin(theta)),othermv),
-        new Crease(b0,a2,othermv),
-        new Crease(a1,a2,startmv),
-        new Crease(a2,new Vertex(a[2]+minlength,0),othermv),
-        new Crease(a2,new Vertex(a2.x-minlength*Math.cos(theta),minlength*Math.sin(theta)),othermv)
-    ])
-    for(const crease of creases){
-        for(const vertex of crease.vertices){
-            if(!vertices.includes(vertex)){
-                vertices.push(vertex)
-            }
-            if(vertex.creases == []){
-                vertex.creases = [crease]
-            } else{
-                vertex.creases.push(crease)
-            }
-        }
-    }
-
-    cp = new CP(vertices,creases)
-    //console.log(cp)
-    normalize(cp)
-    //console.log(cp)
-    displaycp = displayCp(cp,10,10,490,490)
+function step3c(){
+    console.log("step 3c")
 }
-function normalize(cp){
-    `
-    Map the minimum and maximum x to 0 and 1. Center y at 0.5
-    Mutates cp.
-    `
-    var minx = 0
-    var miny = 0
-    var maxx = 0
-    var maxy = 0
-    for(const vertex of cp.vertices){
-        minx = Math.min(minx,vertex.x)
-        maxx = Math.max(maxx,vertex.x)
-        miny = Math.min(miny,vertex.y)
-        maxy = Math.max(maxy,vertex.y)
-    }
-    var scale = (1-0)/(maxx-minx)
-    for(const vertex of cp.vertices){
-        vertex.x = 0 + (vertex.x-minx)*scale
-        vertex.y = 0.5 -vertex.y*scale
-    }
-    return cp
+function step3d(){
+    console.log("step 3d")
 }
